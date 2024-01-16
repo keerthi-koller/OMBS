2ce8a9c799094a151c994caef9c08a175a9bb981
keerthi-koller <keerthi.nk8@gmail.com>
2024-01-16 16:16:14 +0530

    Task 1 done

---
+++ src/main/java/com/jspiders/ombs/TeamProjectOmbsApplication.java
@@ -0,0 +1,15 @@
+package com.jspiders.ombs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class TeamProjectOmbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectOmbsApplication.class, args);
+	}
+
+}
